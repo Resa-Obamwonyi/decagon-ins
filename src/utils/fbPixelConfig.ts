a151f9c3bb7cbcf5c6e7b36f5fc175e1cfa49edb
mrtoluadesina <tadesina90@gmail.com>
2020-07-27 14:40:51 +0100

    fix env

--- src/utils/fbPixelConfig.ts
+++ src/utils/fbPixelConfig.ts
@@ -2,6 +2,7 @@ import ReactPixel from 'react-facebook-pixel';
 
 export default function facebookPixelConversion(page: String, data = {}) {
   ReactPixel.init(
+    //@ts-ignore
     process.env.REACT_APP_FACEBOOK_PIXEL_ID,
     {},
     { debug: false, autoConfig: true },
