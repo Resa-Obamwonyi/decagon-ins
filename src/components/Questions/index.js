43a147e70b165459c5df4ba33496ffe783c06aa7
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-03-16 20:31:16 +0100

    faq page construction

---
+++ src/components/Questions/index.js
@@ -0,0 +1,120 @@
+import React, {useState} from 'react';
+import { QuestionStyle } from './style';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import { faqOne, faqTwo, faqThree } from './data';
+import { AiFillCaretRight, AiFillCaretDown } from 'react-icons/ai';
+
+// const [caret, setCaret] = useState(<AiFillCaretRight />);
+// const CaretToggle = () => {
+//     caret === <AiFillCaretRight /> ? setCaret(...caret, <AiFillCaretDown />) :  setCaret(...caret, <AiFillCaretRight />)
+//     ;
+// }
+
+const faq1 = () => {
+    return (
+      <div className="accordion-div">
+        <Accordion allowZeroExpanded className="accordion">
+          {faqOne.map((faqOne) => (
+            <AccordionItem key={faqOne.question}>
+              <AccordionItemHeading>
+                <AccordionItemButton className="accordion-title">
+                          {faqOne.question}
+                          {/* {CaretToggle} */}
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <hr />
+              <AccordionItemPanel className="accordion-content">
+                {faqOne.answer}
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    );
+}
+
+const faq2 = () => {
+    return (
+      <div className="accordion-div">
+        <Accordion allowZeroExpanded className="accordion">
+          {faqTwo.map((faqTwo) => (
+            <AccordionItem key={faqTwo.question}>
+              <AccordionItemHeading>
+                <AccordionItemButton className="accordion-title">
+                  {faqTwo.question}
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <hr />
+              <AccordionItemPanel className="accordion-content">
+                {faqTwo.answer}
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    );
+};
+
+const faq3 = () => {
+    return (
+      <div className="accordion-div">
+        <Accordion allowZeroExpanded className="accordion">
+          {faqThree.map((faqThree) => (
+            <AccordionItem key={faqThree.question}>
+              <AccordionItemHeading>
+                <AccordionItemButton className="accordion-title">
+                  {faqThree.question}
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <hr />
+              <AccordionItemPanel className="accordion-content">
+                {faqThree.answer}
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    );
+};
+const Questions = () => {
+
+    const [show, setShow] = useState({key:0})
+    const handleClick = (key) => {
+        setShow((prev)=>({...prev,key:key}))
+    }
+
+    return (
+      <QuestionStyle show={show}>
+        <div className="transition-buttons">
+          <button
+            className={`${show.key === 0 ? "active" : null}`}
+            onClick={() => handleClick(0)}
+          >
+            About the Process
+          </button>
+          <button
+            className={`${show.key === 1 ? "active" : null}`}
+            onClick={() => handleClick(1)}
+          >
+            About the Program
+          </button>
+          <button
+            className={`${show.key === 2 ? "active" : null}`}
+            onClick={() => handleClick(2)}
+          >
+            About the Loan
+          </button>
+            </div>
+            {show.key===0?faq1(): show.key===1?faq2(): show.key===2?faq3(): null}
+      </QuestionStyle>
+    );
+}
+
+
+export default Questions
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,6 +1,7 @@
 import "../App.css";
 import Header from "../components/Header";
 import StudentMain from "../components/StudentMain";
+import Questions from "../components/Questions";
 import ApplyBanner from "../components/ApplyBanner";
 import Footer from "../components/Footer";
 
@@ -8,7 +9,8 @@ function FAQ() {
   return (
     <div>
       <Header />
-      <StudentMain heading={"FAQs"} padding={"40px 300px"}/>
+      <StudentMain heading={"FAQs"} padding={"40px 300px"} />
+      <Questions />
       <ApplyBanner />
       <Footer />
     </div>
