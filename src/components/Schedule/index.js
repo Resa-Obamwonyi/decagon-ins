324a276f8fe0fb6d1b6c8cddcda03fcd8747be6a
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-03-19 14:03:52 +0100

    completed table section

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -3,10 +3,55 @@ import { ScheduleStyle } from "./style";
 
 function Schedule() {
   return (
-      <ScheduleStyle>
-          <table>
-              <th>Hi</th>
-          </table>
+    <ScheduleStyle>
+      <table>
+        <tr>
+          <th className="time-row">TIME</th>
+          <th className="activity-row">ACTIVITY</th>
+        </tr>
+        <tr>
+          <td className="time">9am - 11am </td>
+          <td>
+            <h3>Instructor Led Class</h3>
+            <p>
+              Get guidance, feedback, and more from experts who are dedicated to
+              supporting your learning and career goals.
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td className="time">11am - 12 pm</td>
+          <td>
+            <h3>Break</h3>
+          </td>
+        </tr>
+        <tr>
+          <td className="time">12pm - 2pm</td>
+          <td>
+            <h3>Algorithms</h3>
+            <p>
+              Student should be proficient in algorithms, to help them solve
+              real life problems.
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td className="time">2pm - 3pm</td>
+          <td>
+            <h3>Break</h3>
+          </td>
+        </tr>
+        <tr>
+          <td className="time">3pm - 6pm:</td>
+          <td>
+            <h3>Project/Task Deliverables</h3>
+            <p>
+              you build will create an amazing portfolio that showcases your
+              skills and experience.
+            </p>
+          </td>
+        </tr>
+      </table>
     </ScheduleStyle>
   );
 }
--- src/components/Schedule/style.js
+++ src/components/Schedule/style.js
@@ -1,5 +1,37 @@
 import styled from "styled-components";
 
 export const ScheduleStyle = styled.div`
- 
+  background: #fafcf4;
+  th {
+    font-size: 16px;
+    color: #ffffff;
+    background: #34a853;
+    text-align: left;
+    padding: 20px;
+  }
+  .time {
+    background: #34a853;
+    color: #ffffff;
+    font-size: 16px;
+    padding-left: 20px;
+  }
+  table, td{
+    border-top: 1px solid white;
+    border-collapse: collapse;
+  } 
+
+  p {
+    color: #171825;
+    padding-left: 20px;
+  }
+  h3 {
+    color: #171825;
+    padding-left: 20px;
+  }
+  .activity-row {
+    width: 80%;
+  }
+  .time-row {
+    width: 20%;
+  }
 `;
