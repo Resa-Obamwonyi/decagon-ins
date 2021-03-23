94989e88b8a17379e95d08ebe1aca2d2ab72bdf1
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-03-23 06:25:23 +0100

    form submission

---
+++ src/components/Form/validationSchema.js
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+
+export const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required("Name is required.")
+    .min(5, "Full Name is too short, must be at least 5 Characters long."),
+  gender: Yup.string().required("Gender is required."),
+  dob: Yup.string().required("Date of Birth is required."),
+  phone_number: Yup.string().required("Phone Number is required."),
+  email: Yup.string().email().required("Email is required."),
+  state_of_origin: Yup.string().required("State of Origin is required."),
+  current_location: Yup.string().required("Location is required."),
+  highest_qualification: Yup.string().required("Qualification is required."),
+  institution: Yup.string().required("Institute is required."),
+  course_of_study: Yup.string().required("Course is required."),
+  degree: Yup.string().required("Grade is required."),
+  programming_experience: Yup.string().required("Experience is required."),
+  nysc_status: Yup.string().required("NYSC is required."),
+  hear_about_us: Yup.string().required(
+    "Where did you hear about us section is required."
+  ),
+});
--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -1,13 +1,13 @@
 import "../App.css";
 import Header from "../components/Header";
-import Form from "../components/Form";
+import FormDiv from "../components/Form";
 import Footer from "../components/Footer";
 
 function Testimonials() {
   return (
     <div>
       <Header backgroundColor="#2D2F48" color="#ffffff" position="relative" location="apply"/>
-      <Form />
+      <FormDiv />
       <Footer />
     </div>
   );
