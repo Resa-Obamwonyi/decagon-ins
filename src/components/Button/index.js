7c3a557f8634cca675ecf0b42706d25b047262ea
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-04-01 13:33:14 +0100

    added phone number validation and button disabling

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {ButtonStyle} from './style';
 
-const Button=({link, children, width})=> {
+const Button=({link, children, width, disabled})=> {
 
     return (
       <a href={link}>
-        <ButtonStyle width={width} type="submit">
+        <ButtonStyle width={width} type="submit" disabled={disabled}> 
           {children}
         </ButtonStyle>
       </a>
