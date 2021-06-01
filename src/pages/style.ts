d0d9bc7873e5c89749224347145c538427cc28d5
Ebi Aremieye <ebia@decagonhq.com>
2021-06-01 15:15:20 +0100

    fix bugs

---
+++ src/pages/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const IndexStyle = styled.div`
+  padding: 0 15px;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    max-width: 730px;
+  }
+  @media (min-width: 992px) {
+    max-width: 940px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1130px;
+  }
+  @media (min-width: 1360px) {
+    max-width: 1500px;
+  }
+`;
